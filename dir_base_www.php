12513dc9693b82805c99168e1d800be5a6cb4001
unknown <gensukeSpp>
2023-05-16 12:03:03 +0900

    invalid bucket name on php.

--- dir_base_www.php
+++ dir_base_www.php
@@ -2,6 +2,14 @@
 
 	require_once __DIR__ . '/vendor/autoload.php';
 	ini_set( 'display_errors', 1 );
+
+	use Google\Cloud\Storage\StorageClient;
+
+    $storage = new StorageClient();
+    // $storage->registerStreamWrapper();
+
+    $bucket = $storage->bucket('gs://shin-sunkit_bucket');
+    // $content = file_get_contents($bucket);
 	// define( 'SMARTY_DIR', __DIR__ . '/vendor/smarty/smarty/libs/' );
 	// require_once SMARTY_DIR . 'Smarty.class.php';
 	require_once __DIR__ . '/new_menu.php';
@@ -11,7 +19,7 @@
 	$smarty = new Smarty();
 
 	$smarty->template_dir = '/templates/';
-	$smarty->compile_dir  = '/templates_c/';
+	$smarty->compile_dir  = $bucket->upload(fopen('/templates_c/'));
 	$smarty->config_dir   = '/configs/';
 	$smarty->cache_dir    = '/cache/';
 
